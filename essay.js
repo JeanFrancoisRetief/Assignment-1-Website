7bff161540be45240578f3b888fa5a215a0b66fa
JeanFrancoisRetief <2458318@students.wits.ac.za>
2023-05-31 12:18:54 +0200

    added essay

---
+++ essay.js
@@ -0,0 +1,97 @@
+const essayArray = [
+    {
+        "Title": "<h2>Case Study: Takealot – Ethics, UI, UX and Interaction</h2>",
+        "Topic": "<h3>Ethics, UI, UX, and Interaction</h3>",
+        "num": "<p>Essay 1</p>",
+        "par1": "<p>The website that offers services in South Africa that I’ve chosen to discuss is: Takealot, an online marketplace. I chose this website, since my family and I have been using it for years and we have noticed things that work well… and things that don’t.  It is a commonly used website in South Africa (SimilarWeb [1] places it at no. 20  of the most used sites, while SemRush [2] places it at no. 19 in March 2023 – both place Takealot at no. 1 for most used eCommerce & Shopping sites*) and due to this site’s ubiquity in South Africa, I felt it to be a good case study to look at in terms of ethics, UI, UX, and Interaction (it affects many South African lives).</p>",
+        "par2": "<p>*Note: SimilarWeb and SemRush, when visited will show the previous month’s statistics, and at the time of writing this essay the previous month is March 2023, however this placing in the ranks is most likely to stay relatively consistent when checking these tracker sites in later months of this year unless another site becomes wildly popular and overtakes Takealot in the near future.</p>",
+        "par3": "<p>Firstly, I’ll discuss the ethics surrounding the Takealot website, as well as the ethics of the site’s interaction design and UI.</p>",
+        "par4": "<p>To start off, when you simply Google search “online shop” in South Africa one of the first results are sponsored links for Takealot and other big eCommerce sites. While this is more of an ethical issue on Google’s side (changing search engine results for profit), Takealot is still involved, since they pay for the sponsored link. This is likely one of the reasons Takealot is currently (at time of writing this essay) ranked no. 1 among eCommerce & Shopping sites, since most people click on one of the first links given to them by Google’s search engine. In fact, as reported by Search Engine Journal [3], the first link gets over a quarter of search engine users to click it. Sponsored links have become more ubiquitously used by big sites to stay on top of the search engine result list.</p>",
+        "par5": "<p>Now, going into Takealot [4] itself, the first thing you see (which is emphasized by it’s dominating size in the centre of the screen) is sponsored adverts and a list of featured brands. These advertisement’s placement and over prominence on screen draw the user’s attention away from the simple and bland search bar (which is most likely the first thing a user would like to interact with in any site with a huge catalogue of items). While the search bar is not “hidden” it is placed in proximity to other rectangles (that leads to other sections such as “Loadshedding” or “Deals & Promotions”) and all of the similar-looking elements are in bland whites and greys so that the colourful adverts draw the user’s attention easier. The UI is nudging against the user’s best interest and deliberately distracts the user from their original goals when entering the website. These are ethical issues touched upon in Week 7’s lecture slides [5].</p>",
+        "par6": "<p>When searching for items on Takealot, the first items related to your search, like with the above-mentioned Google search results, are marked “Sponsored”. These sponsored items are put in front to catch the attention of the user but quite often might not be exactly what the user is looking for. For example, when I searched “ps5” (on 28 April 2023**) the first two items shown to me is an unofficial cooling stand (not made by Sony) and a Nintendo Switch dock set. These items are not something I would want to purchase when looking for Sony PlayStation products. Especially promoting unofficial third-party accessories before the actual product can nudge users to make a sub-optimal purchasing decision. When I searched “Sony ps5” (on 28 April 2023**), the first two (sponsored) items are headsets not made by Sony. In both searches, only the third item was official Sony PlayStation products.</p>",
+        "par7": "<p>**Note: the sponsored items mentioned in my example change with every search and the sponsors will likely change with time. Recreating the test of searching “ps5” or “Sony ps5” might yield slightly different results.</p>",
+        "par8": "<p>Other ethical issues with Takealot involve the use of dark patterns, specifically having ambiguous sales deadlines [6] (no deadlines obviously given to users) or making it look like an item is on sale, but in actuality: the crossed-out higher price actually being the recommended retailer “list price” and the lower price only being the usual Takealot pricing for the item.*** These practices put false urgency on the user or create the illusion of an opportunity to get an item for cheaper in order to trick the user into considering buying an item they wouldn’t otherwise in other circumstances. While these practices aren’t illegal, especially since they are disclosed in the terms and conditions of the website, it is still unethical. I myself have thought I’ve gotten a good deal on an item from Takealot, but upon investigating the site for this essay, I am now disillusioned with that notion. The main unethical factor of this, is that it took me investigating the site to realize this – I had to click on a small, greyed-out “i”-icon next to the greyed-out, crossed-out pricing to make a pop-up appear, then clicking on the terms and conditions link on the pop-up, then (in the terms of conditions page) using ctrl+F to find-in-page the clause mentioning the “list price” referred to in the pop-up message. That is way too much effort for the average user to go through and this tedious method of getting to the desired clause discourages users from investigating these elements of the site.</p>",
+        "par9": "<p>***Note: The term “list price” is explained in the website’s terms and conditions [7]</p>",
+        "par10": "<p>Now to move on to further discussions on the UI, UX, and Interaction of Takealot and how they can be helpful to the user.</p>",
+        "par11": "<p>From an IxD Process perspective, the goal of the website is to give users (potential online shoppers) access to a shopfront where they can easily search for items (or categories of items) they wish to buy. c, the content mapping of the website is as follows. The homepage showcases featured brands, popular categories, sales, advertisements, and a list of categories. At the top of each page are the navigation (to log-in, register and order pop-up menus as well as the user’s account page) and the search bar. One problem with the search bar being at the top of the page, and not fixed to the user’s scrolling: in order to make a new search the user must scroll all the way back to the top of the current page. Once the user has made a search query, the resulting page shows filters and category refinements on the far left, and then the actual store items make up the majority of the rest of the page. Unlike the homepage, there are no advertisements on the right side of the search results page to allow more room for the store items and the filters. The “advertisements” take the form of sponsored items in between the other store items you’ve searched for.</p>",
+        "par12": "<p>The user flow, at the time of writing this, is as follows: after entering the website the user will see advertisements, a list of categories to shop from, the search bar and simple navigation (to user’s account or shopping cart). The user can then either scroll down to see deals or previously searched (but not bought) items or the user can interact with the search bar or category list to start finding items they wish to buy. After they reach a page (category or specific search result) the player can scroll down to view the different products. Once the user selects a product, they go to that product’s page, where they find more detailed information and reviews on the product. From here the user can add the item to their cart, or investigate similar products that are recommended. Once the user has added all the items they wish to their cart they can navigate to their cart page and proceed to checkout in order to purchase these items (The user will decide payment methods and whether to pick up the item or have it delivered).</p>",
+        "par13": "<p>Looking at some core UI principles and methodologies employed by Takealot. The website, like many online store-fronts, leverages the Zeigarnik effect [8] by making the user have a shopping cart before making a final purpose. The user adds an item to the cart and continues searching for other items, all the while the items in the cart is in the back of the user’s mind and reinforces by the non-zero number in the green cart icon at the top of each page you visit in the website. The user is less likely to forget to buy or to navigate away from the site before making a final purchasing decision. While this is not necessarily a dark UX pattern, since the main purpose of online shopping carts is user convenience when purchasing multiple items at once (or in this site’s case, ordering for one single bulk delivery), it still has a small effect on user’s decision making. Other than this, most core UI principles are upheld quite nicely. The website is consistent, has a simple and understandable UI structure, uses conventions of other online store-fronts, and gives the user the proper amount of control and information of their current situation (such as what’s in their cart, the total price, whether the purchase went through, how long until delivery, notifications of delivery status, etc.) as well as many others discussed during various lectures and readings during the first block.</p>",
+        "par14": "<p></p>",
+        "par15": "<p></p>",
+        "par16": "<p></p>",
+        "par17": "<p></p>",
+        "par18": "<p></p>",
+        "par19": "<p></p>",
+        "par20": "<p></p>",
+    },
+    {
+        "Title": "<h2>Title</h2>",
+        "Topic": "<h3>Topic</h3>",
+        "num": "<p>Essay 2</p>",
+        "par1": "<p>Will apear here</p>",
+        "par2": "<p></p>",
+        "par3": "<p></p>",
+        "par4": "<p></p>",
+        "par5": "<p></p>",
+        "par6": "<p></p>",
+        "par7": "<p></p>",
+        "par8": "<p></p>",
+        "par9": "<p></p>",
+        "par10": "<p></p>",
+        "par11": "<p></p>",
+        "par12": "<p></p>",
+        "par13": "<p></p>",
+        "par14": "<p></p>",
+        "par15": "<p></p>",
+        "par16": "<p></p>",
+        "par17": "<p></p>",
+        "par18": "<p></p>",
+        "par19": "<p></p>",
+        "par20": "<p></p>",
+    }
+];
+
+const referenceArray = [
+    {
+        "num": "<p>Essay 1</p>",
+        "ref1": "<p>[1] 	SimilarWeb, 'Top Websites Ranking - Most Visited Websites in South Africa,' SimilarWeb, 1 April 2023. [Online]. Available: https://www.similarweb.com/top-websites/south-africa/. [Accessed 28 04 2023].</p>",
+        "ref2": "<p>[2] 	SemRush, 'Top websites,' SemRush, 13 April 2023. [Online]. Available: https://www.semrush.com/website/top/south-africa/all/. [Accessed 28 04 2023].</p>",
+        "ref3": "<p>[3] 	M. G. Southern, 'Over 25% of People Click the First Google Search Result,' Search Engine Journal, 14 July 2020. [Online]. Available: https://www.searchenginejournal.com/google-first-page-clicks/374516/#close. [Accessed 28 04 2023].</p>",
+        "ref4": "<p>[4] 	Takealot, 'takealot.com,' Takealot, [Online]. Available: https://www.takealot.com/. [Accessed 28 04 2023].</p>",
+        "ref5": "<p>[5] 	P. Goldberg, 'ETHICS OF UX PRACTICE - YOU, YOUR AUDIENCE AND THE WEIGHT OF EXPERIENCE', p. 12 & 13.</p>",
+        "ref6": "<p>[6] 	R. Steiner, 'Dark Patterns: A New Scientific Look At UX Deception,' Fyresite, 14 Feb 2020. [Online]. Available: https://www.fyresite.com/dark-patterns-a-new-scientific-look-at-ux-deception/. [Accessed 29 04 2023].</p>",
+        "ref7": "<p>[7] 	Takealot, 'T&CS,' Takealot, [Online]. Available: https://www.takealot.com/help/terms-and-conditions#list-price. [Accessed 29 04 2023].</p>",
+        "ref8": "<p>[8] 	L. Dukes, 'Psychology in design: the Zeigarnik effect,' UX Collective, 8 Oct 2020. [Online]. Available: https://uxdesign.cc/psychology-in-design-the-zeigarnik-effect-a59317503f8f. [Accessed 29 04 2023].</p>",
+        "ref9": "<p></p>",
+        "ref10": "<p></p>",
+    }
+];
+
+
+let displayedEssay = document.getElementById("EssayDisplay");
+
+
+//only one essay so far
+let i = 0;
+displayedEssay.innerHTML =  essayArray[i].num + essayArray[i].Title + essayArray[i].Topic + essayArray[i].par1 + essayArray[i].par2 
++ essayArray[i].par3 + essayArray[i].par4 + essayArray[i].par5 + essayArray[i].par6 + essayArray[i].par7 + essayArray[i].par8 + essayArray[i].par9
++ essayArray[i].par10 + essayArray[i].par11 + essayArray[i].par12 + essayArray[i].par13 + essayArray[i].par14 + essayArray[i].par15 + essayArray[i].par16
++ essayArray[i].par17 + essayArray[i].par18 + essayArray[i].par19 + essayArray[i].par20 + "<p>References:</p>" +  referenceArray[i].ref1 + referenceArray[i].ref2
++ referenceArray[i].ref3 + referenceArray[i].ref4 + referenceArray[i].ref5 + referenceArray[i].ref6 + referenceArray[i].ref7 + referenceArray[i].ref8
++ referenceArray[i].ref9 + referenceArray[i].ref10;
+/*
+document.querySelector("input").addEventListener('input', () => getEssay());
+function getEssay()
+{
+    let i = document.querySelector("input").value - 1;
+    if (i > blogArray.length-1)
+    {
+        alert("There are only "  + (essayArray.length) +  " essays");
+    }
+    else
+    {
+        displayedEssay.innerHTML =  essayArray[i].Title + essayArray[i].Topic + essayArray[i].num;
+    }
+    
+    
+}*/
